49199fbc9f5fd437881796cdb373fc78f972f1b0
liubo <zcx.rick@outlook.com>
2023-09-01 16:32:14 +0800

    rename io

--- io.py
+++ io.py
@@ -1,8 +1,8 @@
 # with .. as <==> try .. except .. finally ..
 # returned value assigned to the variable after as
-with open('1.txt', 'w') as f:
+with open('file/1.txt', 'w') as f:
     f.write('abc\ndef')
 
-with open('1.txt', 'r') as f:
+with open('file/1.txt', 'r') as f:
     for line in f.readlines():
         print(line)
