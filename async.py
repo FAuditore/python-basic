4f71a1894ac6f8fcc9829ed101f9f88728c26b5c
liubo <zcx.rick@outlook.com>
2023-09-16 23:27:30 +0800

    async not finish

--- async.py
+++ async.py
@@ -1,9 +1,20 @@
+# asyncio 提供一组 高层级 API 用于:
+#     并发地 运行 Python 协程并对其执行过程实现完全控制
+#     执行 网络 IO 和 IPC
+#     控制 子进程
+#     通过队列 实现分布式任务
+#     同步 并发代码
+
+# 还有一些 低层级 API 以支持库和框架的开发者实现
+#     创建和管理事件循环，以提供异步 API 用于网络化, 运行子进程，处理OS信号等等
+#     使用 transports 实现高效率协议
+#     通过 async/await 语法桥接基于回调的库和代码
+
 import asyncio
 
 import itertools
 import random
 import sys
-import time
 
 
 class Signal:
@@ -31,10 +42,4 @@ def slow_job():
     yield from asyncio.sleep(t)
     return t
 
-
-@asyncio.coroutine
-def supervisor():
-    spinner = asyncio.as(spin('thinking!'))
-
-
-supervisor()
+# TODO not finish
